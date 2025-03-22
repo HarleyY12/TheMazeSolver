d880b4783005d31bcbedb6eeeffec1667a12d947
HarleyY12 <hyang2496@gmail.com>
2025-03-22 12:12:47 -0400

    Update MazeSolver.java
    Working on a method that uses recursion to back track on dead ends

--- src/MazeSolver.java
+++ src/MazeSolver.java
@@ -9,11 +9,15 @@ public class MazeSolver {
         yPos = 0;
         boolean visited[][] = new boolean[array.length][array[0].length];
         System.out.println("(" + xPos + "," + yPos + ")");
+        exploreMaze(array,visited);
+      
 
-       while (xPos < array.length-1||yPos<array[0].length-1)
-       {
-           visited[xPos][yPos] = true;
-           boolean moved = false;
+    }
+    private boolean exploreMaze(String[][]array,boolean[][]visited){
+        while (xPos < array.length-1||yPos<array[0].length-1)
+        {
+            visited[xPos][yPos] = true;
+            boolean moved = false;
             if (xPos+1<array.length && Objects.equals(array[xPos][yPos], array[xPos + 1][yPos]) && !visited[xPos+1][yPos]) {
                 moved = true;
                 xPos = xPos + 1;
@@ -29,13 +33,13 @@ public class MazeSolver {
             }
 
 
-           if(moved){
+            if(moved){
                 System.out.println("--->(" + xPos + "," + yPos + ")");
             }
 
         }
+        return true;
     }
-
 
 
 
